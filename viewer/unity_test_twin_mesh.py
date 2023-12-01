ad2b6cdfca200bfec3b0cfdee36e887a1347cdc6
mschwimmbeck <141571349+mschwimmbeck@users.noreply.github.com>
2023-12-01 14:46:29 +0100

    updating licences

--- viewer/unity_test_twin_mesh.py
+++ viewer/unity_test_twin_mesh.py
@@ -1,3 +1,13 @@
+# ------------------------------------------------------------------------
+# HOLa
+# url: https://github.com/mschwimmbeck/HOLa
+# Copyright (c) 2023 Michael Schwimmbeck. All Rights Reserved.
+# Licensed under the GNU Affero General Public License v3.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# HL2SS 
+# url: https://github.com/jdibenes/hl2ss/tree/main
+# Copyright (c) 2022 by Stevens Institute of Technology. All Rights Reserved. [see licences for details]
+# ------------------------------------------------------------------------
 
 import multiprocessing as mp
 import numpy as np
