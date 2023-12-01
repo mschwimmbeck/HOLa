024c61d358b413d61524346af300ccedb2c21f4a
mschwimmbeck <141571349+mschwimmbeck@users.noreply.github.com>
2023-12-01 14:44:12 +0100

    updating licences

--- viewer/unity_demo_cube.py
+++ viewer/unity_demo_cube.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------
+# HOLa
+# url: https://github.com/mschwimmbeck/HOLa
+# Copyright (c) 2023 Michael Schwimmbeck. All Rights Reserved.
+# Licensed under the GNU Affero General Public License v3.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# HL2SS 
+# url: https://github.com/jdibenes/hl2ss/tree/main
+# Copyright (c) 2022 by Stevens Institute of Technology. All Rights Reserved. [see licences for details]
+# ------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------
 # This script adds a cube to the Unity scene and animates it. Press esc to
 # stop.
