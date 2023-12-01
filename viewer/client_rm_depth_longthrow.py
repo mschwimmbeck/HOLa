b1dd8ace6bc83959ebe9bb4df644acbe6acc7bc3
mschwimmbeck <141571349+mschwimmbeck@users.noreply.github.com>
2023-12-01 14:25:01 +0100

    updating licences

--- viewer/client_rm_depth_longthrow.py
+++ viewer/client_rm_depth_longthrow.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------
+# HOLa
+# url: https://github.com/mschwimmbeck/HOLa
+# Copyright (c) 2023 Michael Schwimmbeck. All Rights Reserved.
+# Licensed under the GNU Affero General Public License v3.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# HL2SS 
+# url: https://github.com/jdibenes/hl2ss/tree/main
+# Copyright (c) 2022 by Stevens Institute of Technology. All Rights Reserved. [see licences for details]
+# ------------------------------------------------------------------------
+
 #------------------------------------------------------------------------------
 # This script receives encoded video from the HoloLens depth camera in long 
 # throw mode and plays it. The resolution is 320x288 @ 5 FPS. The stream
