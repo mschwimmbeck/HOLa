c0e0e8eb192dbbb9986555c9c5b79c2e90f0c7b0
mschwimmbeck <141571349+mschwimmbeck@users.noreply.github.com>
2023-12-01 14:30:22 +0100

    updating licences

--- viewer/demo_video.py
+++ viewer/demo_video.py
@@ -1,3 +1,15 @@
+# ------------------------------------------------------------------------
+# HOLa
+# url: https://github.com/mschwimmbeck/HOLa
+# Copyright (c) 2023 Michael Schwimmbeck. All Rights Reserved.
+# Licensed under the GNU Affero General Public License v3.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# HL2SS 
+# url: https://github.com/jdibenes/hl2ss/tree/main
+# Copyright (c) 2022 by Stevens Institute of Technology. All Rights Reserved. [see licences for details]
+# ------------------------------------------------------------------------
+
+
 #------------------------------------------------------------------------------
 # This script receives encoded video from the HoloLens cameras and plays it.
 # Press esc to stop.
