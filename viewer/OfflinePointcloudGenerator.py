cd58b676e1aa58b1df0927922849ded0fd5c2d85
mschwimmbeck <141571349+mschwimmbeck@users.noreply.github.com>
2023-12-01 14:19:30 +0100

    updating licences

--- viewer/OfflinePointcloudGenerator.py
+++ viewer/OfflinePointcloudGenerator.py
@@ -1,3 +1,13 @@
+# ------------------------------------------------------------------------
+# HOLa
+# url: https://github.com/mschwimmbeck/HOLa
+# Copyright (c) 2023 Michael Schwimmbeck. All Rights Reserved.
+# Licensed under the GNU Affero General Public License v3.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from HL2SS (https://github.com/jdibenes/hl2ss/tree/main)
+# Copyright (c) 2022 by Stevens Institute of Technology. All Rights Reserved. [see licences for details]
+# ------------------------------------------------------------------------
+
 import open3d as o3d
 import hl2ss
 import hl2ss_3dcv
